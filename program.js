fb48a5c1945568730eac76c40290c31cf5cad2dc
chrisfitkin <cfitkin@gmail.com>
2016-09-08 22:32:32 -0700

    more streaming

--- program.js
+++ program.js
@@ -1,4 +1,18 @@
 // const fs = require('fs');
 // fs.createReadStream(process.argv[2]).pipe(process.stdout);
 
-process.stdin.pipe(process.stdout);
+// process.stdin.pipe(process.stdout);
+
+var through = require('through2');
+var stream = through(write, end);
+
+process.stdin.pipe(stream).pipe(process.stdout);
+
+function write(buffer, encoding, next) {
+  this.push(buffer.toString().toUpperCase());
+  next();
+}
+
+function end(done) {
+  done();
+}
